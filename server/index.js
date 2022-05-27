1059697f506c41ed057018cc6036fd937fb99ec4
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-05-27 17:00:35 -0300

    index

--- server/index.js
+++ server/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2')
 const cors = require('cors');
-const { application } = require('express');
 
+const port = 3001
 const database = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "Lucas123963",
     database: "crud"
 })
-
-app.listen(3001, () => {
-    console.log('Iniciado')
+app.get('/', function (req, res) {
+    res.send("Servidor Rodando")
+})
+app.listen(port, () => {
+    console.log('Iniciado na porta' + port)
 })
 app.use(cors())
 app.use(express.json())
@@ -23,22 +25,11 @@ app.get('/games', (req, res) => {
     database.query(SQL, (err, result) => {
         if (err) {
             console.log(err)
-            return response.status(404).end()
+            return res.status(404).end()
         }
         res.send(result)
     })
 })
-app.put('/games/:id', (req, res) => {
-    const {
-        id,
-        name,
-        price,
-        category
-    } = req.body
-    res.send(id, name, price, category)
-
-})
-
 app.post('/games', (req, res) => {
     const { name } = req.body;
     const { price } = req.body;
@@ -49,4 +40,36 @@ app.post('/games', (req, res) => {
     database.query(SQL, [name, price, category], (err, result) => {
         console.log(err)
     })
+})
+
+app.put('/games/:id', (req, res) => {
+    const {
+        id,
+        name,
+        price,
+        category
+    } = req.body
+    let SQL = "UPDATE games SET name = ?,price = ?, category = ? WHERE idgames = ?";
+
+    database.query(SQL, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).end()
+        }
+        res.send(result)
+    })
+
+})
+
+app.delete('/games/:id', (req, res) => {
+    const { id } = req.params
+
+    let SQL = "DELETE FROM games WHERE idgames = ?"
+    database.query(SQL, [id], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    })
+
 })
